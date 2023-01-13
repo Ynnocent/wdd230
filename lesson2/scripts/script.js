b8c975a72121dbd52b0535da7882a67aeb735c39
Ynnocent <101958912+Ynnocent@users.noreply.github.com>
2023-01-12 17:39:05 -0700

    Progresive Development

---
+++ lesson2/scripts/script.js
@@ -0,0 +1,11 @@
+const today = new Date()
+
+const dateoptions = options = {
+    year:"2-digit",
+    month: "numeric",
+    day: "numeric"
+}
+
+document.querySelector("#currentyear").textContent = today.getFullYear('en-US')
+document.querySelector("#lastmodified").textContent = document.lastModified
+
