006ec47f24b3202f5637c81e080f258e21ef17f0
Ynnocent <101958912+Ynnocent@users.noreply.github.com>
2023-02-14 13:34:07 -0700

    windemp

--- chamber/scripts/windtemp.js
+++ chamber/scripts/windtemp.js
@@ -1,18 +1,24 @@
 function setWindChill (temperature, windspeed) {
-    tempSpan = document.querySelector("#temperatureSpan");
-    windChillSpan = document.querySelector("#windchillSpan");
-    windSpeedSpan = document.querySelector("#windspeedSpan");
+    let tempSpan = document.querySelector("#tempertatureSpan");
+    let windChillSpan = document.querySelector("#windchillSpan");
+    let windSpeedSpan = document.querySelector("#windspeedSpan");
+
+
 
     let windchill = 'N/A';
 
     if (windspeed >= 3.0 && temperature <= 50) {
-        let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+        let chill = Math.round((35.74 + (0.6215 * temperature))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temperature*Math.pow(windspeed,0.16)));
         windchill = `${chill}`;
     }
+
     
     tempSpan.textContent = temperature;
     windChillSpan.innerHTML = windchill;
     windSpeedSpan.textContent = windspeed;
+    // tempSpan.textContent = temperature;
+    // windChillSpan.innerHTML = windchill;
+    // windSpeedSpan.textContent = windspeed;
 
 }
 
