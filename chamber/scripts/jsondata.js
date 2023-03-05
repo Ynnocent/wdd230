a1ab95b8a112bbfcd480ae4cc4a142666f2332bc
Ynnocent <plucenaynno@gmail.com>
2023-03-04 22:07:51 -0700

    directory page

---
+++ chamber/scripts/jsondata.js
@@ -0,0 +1,65 @@
+const dataURL = './data/data.json';
+const directoryContent = document.querySelector("#directory-container");
+const toggleButton = document.querySelector("#toggle-view")
+
+async function getBusiness() {
+    const response = await fetch(dataURL);
+    const data = await response.json();
+    return data
+}
+
+async function listBusinesses() {
+    
+    const dataResponse = await getBusiness()
+
+    for (let i = 0; i < dataResponse.length; i++) {    
+        
+        let businessObjects = await destructureObjects(dataResponse[i])
+        businessObjects.classList.add("business-card")
+        
+        
+        directoryContent.appendChild(businessObjects)
+    }
+}
+
+function destructureObjects({name, address, number, url, membership}) {
+    const newUlObjects = document.createElement("ul");
+
+    newUlObjects.innerHTML = `<img src="${"./images/webp/shirelogo.webp"}">
+     <li>${name}</li>
+     <li>${number}</li>
+     <li>${address}</li>
+     <li>${url}</li>
+     `;
+    if (membership == "gold") {
+        newUlObjects.setAttribute("id","goldmembership")
+    }
+    return newUlObjects;
+}
+
+function handleButton(button) {
+    button.addEventListener("click", () => {
+        if (button.classList.contains('list-view')) {
+            button.classList.add("card-view");
+            button.classList.remove("list-view");
+            button.innerHTML = "CARD";
+
+            directoryContent.classList.add("in-card-mode")
+            directoryContent.classList.remove("in-list-mode")
+        } else {
+            button.classList.add("list-view");
+            button.classList.remove("card-view");
+            button.innerHTML = "LIST";
+
+            directoryContent.classList.add("in-list-mode")
+            directoryContent.classList.remove("in-card-mode")
+        }
+        
+        
+    })
+}
+
+
+
+handleButton(toggleButton)
+listBusinesses()
