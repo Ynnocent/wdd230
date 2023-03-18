da4e04db9c5bed6e3d94f7795c9411cb03ed7903
Ynnocent <plucenaynno@gmail.com>
2023-03-18 03:21:10 -0600

    Final Shire Chamber of Commerce

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -10,13 +10,13 @@ document.querySelector("#currentyear").textContent = today.getFullYear('en-US')
 document.querySelector("#lastmodified").textContent = document.lastModified
 
 const dateoptionsHeader = options = {
-    year:"numeric",
-    weekday:"long",
-    month:"long",
-    day:"numeric"
-}
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+};
 
-document.querySelector("#dateTime").textContent = today.toLocaleDateString('en-US',dateoptionsHeader)
+document.querySelector("#dateTime").textContent = today.toLocaleDateString('en-US', dateoptionsHeader);
 
 // Toggle the menu open or closed
 function toggleMenu(){
